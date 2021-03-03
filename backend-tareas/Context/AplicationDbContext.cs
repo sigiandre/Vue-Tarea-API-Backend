df84ca3f21c3f3d37172a6f644e5266af1d7c7f8
Andre Diaz <andrediaz@MacBook-Pro-de-Andre.local>
2021-03-03 02:20:03 -0500

    tarea backend commit

---
+++ backend-tareas/Context/AplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using backend_tareas.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend_tareas.Context
+{
+    public class AplicationDbContext : DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Tarea> Tareas { get; set; }
+    }
+}
---
+++ backend-tareas/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend_tareas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        // GET: api/<DefaultController>
+        [HttpGet]
+        public string Get()
+        {
+            return "Aplicacion corriendo..";
+        }
+    }
+}
